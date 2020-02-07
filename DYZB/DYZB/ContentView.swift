1d982830755affef22b5b0eb320b0213180bcc2f
小坏蛋 <www.weiaiziqi1314@icloud.com>
2020-02-07 17:54:36 +0800

    初始化项目

---
+++ DYZB/DYZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DYZB
+//
+//  Created by 小坏蛋 on 2020/2/7.
+//  Copyright © 2020 小坏蛋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
